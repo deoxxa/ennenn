08cd4d06edad3e0a9222ae4d48fa40db333a4cc6
deoxxa <deoxxa@fknsrs.biz>
2012-12-29 13:15:22 +0900

    removed commented code from request example

--- example-request.js
+++ example-request.js
@@ -3,17 +3,7 @@
 var ennenn = require("./index");
 
 var parser = new ennenn.RequestParser(function on_request(request) {
-  console.log("[Request started] " + request.command + ": " + request.arguments.join(", "));
-
-/*
-  request.on("data", function on_data(chunk) {
-    console.log("[Request data] " + chunk.length);
-  });
-
-  request.on("end", function on_end() {
-    console.log("[Request ended]");
-  });
-*/
+  console.log("[Request] " + request.command + ": " + request.arguments.join(", "));
 });
 
 parser.write("GET a b c\r\n");
